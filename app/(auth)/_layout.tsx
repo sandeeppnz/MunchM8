683ed5dd2cff00706a436d81ec80b447134951e3
Sandeep Perera <Sandeep.Perera@fastway.co.nz>
2025-08-24 08:48:09 +1200

    Add auth pages

---
+++ app/(auth)/_layout.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const _Layout = () => {
+  return (
+    <View>
+      <Text>Auth layout</Text>
+    </View>
+  )
+}
+
+export default _Layout
