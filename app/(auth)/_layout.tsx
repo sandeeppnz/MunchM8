8b9d2786d6cccc7a9a58a5b591ba32e8b825f1e9
Sandeep Perera <Sandeep.Perera@fastway.co.nz>
2025-08-24 18:33:30 +1200

    Add layout

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,17 +1,42 @@
-import { Slot } from 'expo-router'
-import React from 'react'
-import { Text } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { images } from "@/constants";
+import React from "react";
+import {
+  Dimensions,
+  Image,
+  ImageBackground,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  View,
+} from "react-native";
 
 const _Layout = () => {
   return (
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      <ScrollView
+        className="bg-white h-full"
+        keyboardShouldPersistTaps="handled"
+      >
+        <View
+          className="w-full relative"
+          style={{ height: Dimensions.get("screen").height / 2.25 }}
+        >
+          <ImageBackground
+            source={images.loginGraphic}
+            className="size-full rounded-d-lg"
+            resizeMode="stretch"
+          />
+          <Image
+            source={images.logo}
+            className="self-center size-48 absolute -bottom-16 z-10"
+          />
+        </View>
+      </ScrollView>
+    </KeyboardAvoidingView>
+    // <Slot />
+  );
+};
 
-    <SafeAreaView>
-      <Text>Auth layout</Text>
-      <Slot />
-    </SafeAreaView>
-
-  )
-}
-
-export default _Layout
+export default _Layout;
