3164ca6215464770a95233cd26d8fd2e36847eb5
Sandeep Perera <Sandeep.Perera@fastway.co.nz>
2025-08-24 20:42:02 +1200

    Add endpoint to getCurrentUser

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,5 +1,5 @@
 import { CreateUserParams, SignInParams } from "@/type";
-import { Account, Avatars, Client, Databases, ID } from "react-native-appwrite";
+import { Account, Avatars, Client, Databases, ID, Query } from "react-native-appwrite";
 
 export const appwriteConfig = {
     endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT!,
@@ -49,6 +49,26 @@ export const createUser = async ({ email, password, name }: CreateUserParams) =>
     try {
         const session = await account.createEmailPasswordSession(email, password);
     } catch (e) {
+        throw new Error(e as string);
+    }
+}
+
+export const getCurrentUser = async () => {
+    try {
+        const currentAccount = await account.get();
+        if(!currentAccount) throw Error;
+
+        const currentUser = await databases.listDocuments(
+            appwriteConfig.databaseId,
+            appwriteConfig.userCollectionId,
+            [Query.equal('accountId', currentAccount.$id)]
+        )
+
+        if(!currentUser) throw Error;
+
+        return currentUser.documents[0];
+    } catch (e) {
+        console.log(e);
         throw new Error(e as string);
     }
 }
